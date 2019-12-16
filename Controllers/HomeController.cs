c8d5e79daac3ba3a24ef88e451e96aa96610b19b
Nick Usov <nicky.yalta.ua@gmail.com>
2019-12-16 09:32:03 +0200

    calculator

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication3.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Calc(double a, double b, string action)
+        {
+            ViewBag.a = a;
+            ViewBag.b = b;
+            ViewBag.action = action;
+            switch (action)
+            {
+                case "+": { ViewBag.result = a + b; } break;
+                case "-": { ViewBag.result = a - b; } break;
+                case "*": { ViewBag.result = a * b; } break;
+                case "/": { ViewBag.result = a / b; } break;
+                default: ViewBag.result = ""; break;
+            }
+            return View();
+        }
+    }
+}
